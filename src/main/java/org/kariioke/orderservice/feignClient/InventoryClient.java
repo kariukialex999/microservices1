9416e79861458a2f97aa5e16c0f7bd34a9799d08
alex kariuki <kariukialex999@gmail.com>
2026-04-16 16:48:38 +0300

    final commit

--- src/main/java/org/kariioke/orderservice/feignClient/InventoryClient.java
+++ src/main/java/org/kariioke/orderservice/feignClient/InventoryClient.java
@@ -2,19 +2,16 @@ package org.kariioke.orderservice.feignClient;
 
 import org.kariioke.orderservice.dto.InventoryItemResponse;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @FeignClient(name = "inventory-service")
 public interface InventoryClient {
     @GetMapping("/inventory/{id}")
     InventoryItemResponse getItemById(@PathVariable("id") Long id);
 
-    @PatchMapping("/inventory/{id}/reduce")
+    @PutMapping("/inventory/{id}/reduce")
     InventoryItemResponse reduceStock(@PathVariable("id") Long id, @RequestParam int quantity);
 
-    @PatchMapping("/inventory/{id}/restore")
+    @PutMapping("/inventory/{id}/restore")
     InventoryItemResponse restoreStock(@PathVariable("id") Long id, @RequestParam int quantity);
 }
